f0f1a726d084a80a46e0856f62fd4c8d9a454bd4
Mauricio Giaconia <mauriciogiaco@gmail.com>
2023-05-26 03:19:10 -0300

    Created FaButton component, this component use Font awesome icons!

---
+++ portfolio/src/components/FaButton/FaButton.jsx
@@ -0,0 +1,41 @@
+import styles from './FaButton.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import 'animate.css';
+import { useState } from 'react';
+
+export default function FaButton(props){
+
+    const [hidden, setHidden] = useState(false);
+
+    const handleAnimationEnd = (isHidden) => {
+      setHidden(isHidden);
+    };
+
+    const handleOnClick = () =>{
+
+        if (hidden){
+            handleAnimationEnd(false);
+        }
+        
+        props.onClick();
+    }
+
+    console.log(props.isSelected, props.data, hidden)
+
+    return  <>
+        <div onClick={handleOnClick} className={`${styles.customButton} ${props.isSelected ? styles.selected : ''}`}>
+            <FontAwesomeIcon icon={props.faIcon} />
+        </div>
+
+        <div className={`animate__animated ${props.isSelected ? 'animate__backInLeft' : `animate__backOutRight`} ${styles.dataContainer} ${hidden ? styles.hidden : ''}`} onAnimationEnd={() => {
+            if (!props.isSelected){
+                handleAnimationEnd(true);
+            } else{
+                handleAnimationEnd(false);
+            }
+        }}>
+            <p>{props.data}</p>
+        </div>
+    </>
+    
+}
