552f906a67809d177ab2f5917db47845b0f33840
Mauricio Giaconia <mauriciogiaco@gmail.com>
2023-05-28 06:06:47 -0300

    Added Exhibitor component (component to show the projects)

---
+++ portfolio/src/components/Exhibitor/Exhibitor.jsx
@@ -0,0 +1,55 @@
+import styles from './Exhibitor.module.css';
+import {Carousel} from 'react-responsive-carousel';
+
+import videoSrc from '../../assets/projects/h2h/h2h-video.mp4'
+
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+export default function Exhibitor(props){
+
+    const isImage = /\.(jpg|jpeg|png|gif)$/i;
+    const isVideo = /\.(mp4|mov|avi)$/i;
+
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      };
+   
+    return <div className={`${styles.exhibitorContainer}`}>
+
+        <div className={`${styles.carouselContainer}`}> 
+            <Carousel className={`${styles.customCarousel}`}>
+
+                {props.source.map((item, index) => {
+                    return <div className={`${styles.carouselContent}`} key={index}>
+                        {
+                        <img src={item} alt={props.title} />
+                        }
+                    </div>
+                })}
+
+                <div className={`${styles.carouselContent}`}>
+                    <video controls>
+                        <source src={videoSrc} type="video/mp4" />
+                    </video>
+                </div>
+            </Carousel>
+        </div>
+
+        <div>
+            <h2>{props.title}</h2>
+            <p>{props.description}</p>
+
+            <div>
+                {props.tech.map((item, index) => {
+                    return <img key={index} src={item.icon} alt={item.name} />
+                })}
+            </div> 
+        </div>
+    </div>
+}
